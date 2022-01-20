b2d9223654812a09c30a08f79ea9ccd069350fcb
Jean Barcellos <jeanbarcellos@hotmail.com>
2022-01-20 12:13:30 -0300

    ajustes em pages e store

--- src/store/index.js
+++ src/store/index.js
@@ -1,51 +1,7 @@
 /*
  * NAVIGATION
  */
-export const navPrimary = [
-  {
-    id: 1,
-    icon: 'dashboard',
-    text: 'Dashboard'
-  },
-  {
-    id: 2,
-    icon: 'shopping_cart',
-    text: 'Orders'
-  },
-  {
-    id: 3,
-    icon: 'people',
-    text: 'Customers'
-  },
-  {
-    id: 4,
-    icon: 'bar_chart',
-    text: 'Reports'
-  },
-  {
-    id: 5,
-    icon: 'layers',
-    text: 'Integrations'
-  }
-]
 
-export const navSecondary = [
-  {
-    id: 6,
-    icon: 'assignment',
-    text: 'Current month'
-  },
-  {
-    id: 7,
-    icon: 'assignment',
-    text: 'Last quarter'
-  },
-  {
-    id: 8,
-    icon: 'assignment',
-    text: 'Year-end sale'
-  }
-]
 
 // Generate Sales Data
 const createDataChart = (time, amount) => {
